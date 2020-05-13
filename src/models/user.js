62505cc4757b3b3372e2b5202b2e66ba42248593
ajimae <meeky.ae@gmail.com>
2020-05-13 16:02:53 +0100

    add user authentication functionality

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   firstName: String,
@@ -19,6 +20,34 @@ const userSchema = new mongoose.Schema({
     required: true,
     default: 'regular'
   }
+});
+
+/**
+ * @description encrypt and store user password
+ *
+ * @param save, callback
+ */
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // generate a hashing salt
+  bcrypt.genSalt(10, function(error, salt) {
+    if (error) {
+      throw new Error('error occurred while encryting password');
+    }
+
+    // hash password using generated salt
+    bcrypt.hash(user.password, salt, function(error, hash) {
+      if (error) {
+        throw new Error('an error occurred while encryting password');
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const User = mongoose.model('User', userSchema);
